042aaeb2c5ccff3725dbbefca4ea46e8bda0072d
Trecia Kat <tksekgalo@gmail.com>
2023-02-23 01:01:37 +0200

    removed ts error by creating stateless funcs

---
+++ src/pages/Biography.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const Biography = (props: Props) => {
+  return (
+    <div>Biography</div>
+  )
+}
+
+export default Biography
---
+++ src/pages/Socials.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const Socials = (props: Props) => {
+  return (
+    <div>Socials</div>
+  )
+}
+
+export default Socials
