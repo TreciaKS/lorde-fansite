b36fea980b761454929410d4efc40dacd8cda339
Trecia Kat <tksekgalo@gmail.com>
2023-02-22 23:57:11 +0200

    Added new props: Track tile + description

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,16 +3,20 @@ import { ReactElement } from "react";
 type Props = {
   title: string;
   description: string;
+  favoriteTrackTitle: string;
+  favouriteTrackDescription: string;
 };
 
 // use both css classes and tailwind css as styles
 // returning only an element
-const About = ({ title, description }: Props): ReactElement => {
+const About = ({ title, description , favoriteTrackTitle , favouriteTrackDescription}: Props): ReactElement => {
   return (
     <>
       <section>
         <h1>{title}</h1>
         <p>{description}</p>
+        <p>{favoriteTrackTitle}</p>
+        <p>{favouriteTrackDescription}</p>
       </section>
     </>
   );
