dcb453c1e049741cda8556e41dc61d03ab3a5de2
Trecia Kat <tksekgalo@gmail.com>
2023-03-02 17:38:14 +0200

    set custom colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      "melo-night-blue": "#252637",
+      "melo-rainy-blue": "#49598E",
+      "melo-sky-blue": "#61493A",
+      "pure-white": "#E8E9E9",
+      "pure-black": "#171818",
+      "solar-brown": "#89450E",
+      "solar-coffee": "#61493A"
+    },
     extend: {},
   },
   plugins: [],
