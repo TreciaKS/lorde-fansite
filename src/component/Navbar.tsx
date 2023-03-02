16601894ceef46c78b34a35d2e75f32be2ff6ca8
Trecia Kat <tksekgalo@gmail.com>
2023-03-02 21:00:51 +0200

    styled navbar, fix issue

--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -1,11 +1,101 @@
-import React from 'react'
+import { NavLink } from "react-router-dom"
+import LoginBtn from "./LoginBtn";
+import LogoutBtn from "./LogoutBtn"
 
-type Props = {}
-
-const Navbar = (props: Props) => {
+const Navbar = (): JSX.Element => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <>
+      <header className="relative z-20 w-fullbg-white/90 after:absolute after:top-full after:left-0 after:z-10 after:block after:h-px after:w-full after:bg-slate-200 lg:backdrop-blur-sm lg:after:hidden">
+        <div className="relative mx-auto max-w-full px-6 lg:max-w-5xl xl:max-w-7xl 2xl:max-w-[96rem]">
+          <nav className="flex h-[5.5rem] items-stretch justify-between font-medium text-slate-700">
+            {/* logo */}
+            <div className="flex items-center gap-2 py-3 text-lg whitespace-nowrap focus:outline-none lg:flex-1">
+              <svg
+                width="300"
+                height="300"
+                viewBox="0 0 300 300"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-12 h-12 bg-pink-500"
+              >
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M88.1121 88.1134L150.026 150.027L150.027 150.027L150.027 150.027L150.028 150.027L150.027 150.026L88.1133 88.1122L88.1121 88.1134ZM273.878 273.877C272.038 274.974 196.128 319.957 165.52 289.349L88.1124 211.942L26.1434 273.911C26.1434 273.911 -20.3337 196.504 10.651 165.519L88.1121 88.1134L26.1417 26.1433C26.1417 26.1433 69.6778 0.00338007 104.519 0H0V300H300V0H104.533C116.144 0.00112664 126.789 2.90631 134.534 10.651L211.941 88.1123L273.877 26.177C274.974 28.0159 319.957 103.926 289.349 134.535L211.942 211.942L273.878 273.877ZM273.878 273.877L273.912 273.857V273.911L273.878 273.877ZM273.877 26.177L273.911 26.1429H273.857C273.857 26.1429 273.863 26.1544 273.877 26.177Z"
+                  fill="white"
+                />
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M0 0H300V300H0V0ZM150.026 150.025C121.715 99.731 88.1131 88.1122 88.1131 88.1122L10.6508 165.519C10.6508 165.519 26.143 150.027 150.026 150.027H150.027C150.026 150.027 150.026 150.027 150.026 150.027L150.026 150.027C99.731 178.339 88.1124 211.941 88.1124 211.941L165.52 289.348C165.52 289.348 150.032 273.86 150.027 150.027H150.029C178.341 200.323 211.944 211.942 211.944 211.942L289.352 134.535C289.352 134.535 273.864 150.023 150.027 150.027V150.027L150.027 150.027C200.322 121.715 211.941 88.1125 211.941 88.1125L134.534 10.651C134.534 10.651 150.026 26.1431 150.026 150.025ZM150.027 150.027L150.026 150.027C150.026 150.026 150.026 150.026 150.026 150.025C150.026 150.025 150.027 150.026 150.027 150.027ZM150.027 150.027L150.027 150.026L150.027 150.027C150.027 150.027 150.027 150.027 150.027 150.027L150.027 150.027ZM150.027 150.027C150.027 150.027 150.027 150.027 150.027 150.027H150.027L150.027 150.027Z"
+                  fill="rgba(255,255,255,.2)"
+                />
+              </svg>
+            </div>
 
-export default Navbar
+            {/* responsive menu */}
+            <button className="relative self-center order-10 visible block w-10 h-10 opacity-100 lg:hidden">
+              <div className="absolute w-6 transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+                <span
+                  aria-hidden="true"
+                  className="absolute block h-0.5 w-9/12 -translate-y-2 transform rounded-full bg-slate-900 transition-all duration-300"
+                ></span>
+                <span
+                  aria-hidden="true"
+                  className="absolute block h-0.5 w-6 transform rounded-full bg-slate-900 transition duration-300"
+                ></span>
+                <span
+                  aria-hidden="true"
+                  className="absolute block h-0.5 w-1/2 origin-top-left translate-y-2 transform rounded-full bg-slate-900 transition-all duration-300"
+                ></span>
+              </div>
+            </button>
+
+            <ul className="max-lg:left-72 max-lg:border-2 opacity-0 max-lg:z-20 max-lg:hover:opacity-100 max-lg:min-w absolute top-0 left-0 z-[-1] ml-auto h-screen w-full justify-center overflow-y-auto overscroll-contain bg-white/90 px-8 pb-12 pt-28 font-medium transition-[opacity,visibility] duration-300 lg:visible lg:relative lg:top-0 lg:z-0 lg:flex lg:h-full lg:w-auto lg:items-stretch lg:overflow-visible lg:bg-white/0 lg:px-0 lg:py-0 lg:pt-0 lg:opacity-100">
+              <NavLink to="/" className="flex items-stretch">
+                <li className="flex items-center gap-2 py-4 transition-colors duration-300 focus:bg-emerald-50 focus:outline-none focus-visible:outline-none lg:px-8">
+                  <span>Home</span>
+                </li>
+              </NavLink>
+              <NavLink to="/about" className="flex items-stretch">
+                <li className="flex items-center gap-2 py-4 transition-colors duration-300 focus:bg-emerald-50 focus:outline-none focus-visible:outline-none lg:px-8">
+                  <span>About</span>{" "}
+                </li>
+              </NavLink>
+              <NavLink to="/biography" className="flex items-stretch">
+                <li className="flex items-center gap-2 py-4 transition-colors duration-300 focus:bg-emerald-50 focus:outline-none focus-visible:outline-none lg:px-8">
+                  <span>Biography</span>{" "}
+                </li>
+              </NavLink>
+              <NavLink to="/albums" className="flex items-stretch">
+                <li className="flex items-center gap-2 py-4 transition-colors duration-300 focus:bg-emerald-50 focus:outline-none focus-visible:outline-none lg:px-8">
+                  <span>Albums</span>{" "}
+                </li>
+              </NavLink>
+              <NavLink to="/gallery" className="flex items-stretch">
+                <li className="flex items-center gap-2 py-4 transition-colors duration-300 focus:bg-emerald-50 focus:outline-none focus-visible:outline-none lg:px-8">
+                  <span>Gallery</span>{" "}
+                </li>
+              </NavLink>
+              <NavLink to="/socials" className="flex items-stretch">
+                <li className="flex items-center gap-2 py-4 transition-colors duration-300 focus:bg-emerald-50 focus:outline-none focus-visible:outline-none lg:px-8">
+                  <span>Socials</span>{" "}
+                </li>
+              </NavLink>
+              <LoginBtn />
+              <LogoutBtn />
+            </ul>
+
+            <div className="flex items-center justify-end px-6 ml-auto lg:ml-0 lg:flex-1 lg:p-0">
+              <span className="inline-flex items-center justify-center gap-1 px-4 py-1 text-sm text-white bg-pink-500 rounded-full shadow-sm">
+                Trecia
+              </span>
+            </div>
+          </nav>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Navbar;
