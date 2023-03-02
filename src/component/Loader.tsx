b1a115b556d06f22d229e3e6750275459db18910
Trecia Kat <tksekgalo@gmail.com>
2023-03-02 17:02:10 +0200

    Lottie animation working

--- src/component/Loader.tsx
+++ src/component/Loader.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import { Player } from "@lottiefiles/react-lottie-player";
 
 type Props = {}
 
 const Loader = (props: Props) => {
   return (
-    <div>Loader</div>
-  )
+    <section className="grid place-items-center">
+      <Player
+        autoplay
+        loop
+        src="https://assets6.lottiefiles.com/private_files/lf30_HGg7Oa.json"
+        style={{ height: "300px", width: "300px" }}
+      ></Player>
+    </section>
+  );
 }
 
 export default Loader
