3008cf538108a8366732b470d63c2dbb962b7d04
Trecia Kat <tksekgalo@gmail.com>
2023-03-03 18:03:51 +0200

    imported auth loading components

--- src/App.tsx
+++ src/App.tsx
@@ -12,14 +12,16 @@ import { Route, Routes } from "react-router-dom";
 
 // Auth0
 import { useAuth0 } from "@auth0/auth0-react"
+import AuthLoading from "./auth/AuthLoading";
+import LoginError from "./auth/LoginError";
 
 function App(): JSX.Element {
   const { isLoading, error} = useAuth0()
 
   return (
     <>
-      {error && <p>Failed Authorisation!!!</p>}
-      {!error && isLoading && <p>Authenticating...</p>}
+      {error && <p>{<LoginError />}</p>}
+      {!error && isLoading && <p>{<AuthLoading />}</p>}
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
