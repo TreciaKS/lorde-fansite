d78ba0dd3aba9dce9591703fe4aeb36a6d0166ee
Trecia Kat <tksekgalo@gmail.com>
2023-03-03 18:02:42 +0200

    auth loading component ready

--- src/auth/AuthLoading.tsx
+++ src/auth/AuthLoading.tsx
@@ -2,8 +2,15 @@ import { Player } from "@lottiefiles/react-lottie-player"
 
 const AuthLoading = (): JSX.Element => {
   return (
-    <div>AuthLoading</div>
-  )
+    <section className="grid h-screen place-items-center">
+      <Player
+        autoplay
+        loop={false}
+        src="https://assets2.lottiefiles.com/packages/lf20_rdkzbvu0.json"
+        style={{ height: "300px", width: "300px" }}
+      ></Player>
+    </section>
+  );
 }
 
 export default AuthLoading
--- src/auth/LoginError.tsx
+++ src/auth/LoginError.tsx
@@ -2,8 +2,15 @@ import { Player } from "@lottiefiles/react-lottie-player"
 
 const LoginError = (): JSX.Element => {
   return (
-    <div>LoginError</div>
-  )
+    <section className="grid h-screen place-items-center">
+      <Player
+        autoplay
+        loop={false}
+        src=" https://assets2.lottiefiles.com/packages/lf20_cdvzedxl.json"
+        style={{ height: "300px", width: "300px" }}
+      ></Player>
+    </section>
+  );
 }
 
 export default LoginError
