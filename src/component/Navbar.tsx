f52be122cc005a256df60f77aad7180e34bc03b3
Trecia Kat <tksekgalo@gmail.com>
2023-03-03 17:17:39 +0200

    moved auth btns to navbar

--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -1,8 +1,15 @@
-import { NavLink } from "react-router-dom"
-import LoginBtn from "./LoginBtn";
-import LogoutBtn from "./LogoutBtn"
+// Links
+import { NavLink } from "react-router-dom";
+
+// Authentication
+import { useAuth0 } from "@auth0/auth0-react";
+import LoginBtn from "../auth/LoginBtn";
+import LogoutBtn from "../auth/LogoutBtn";
+import User from "../auth/User";
 
 const Navbar = (): JSX.Element => {
+  const { isAuthenticated, error, isLoading } = useAuth0();
+
   return (
     <>
       <header className="relative z-20 w-fullbg-white/90 after:absolute after:top-full after:left-0 after:z-10 after:block after:h-px after:w-full after:bg-slate-200 lg:backdrop-blur-sm lg:after:hidden">
@@ -51,46 +58,52 @@ const Navbar = (): JSX.Element => {
               </div>
             </button>
 
-            <ul className="max-lg:left-72 max-lg:border-2 opacity-0 max-lg:z-20 max-lg:hover:opacity-100 max-lg:min-w absolute top-0 left-0 z-[-1] ml-auto h-screen w-full justify-center overflow-y-auto overscroll-contain bg-white/90 px-8 pb-12 pt-28 font-medium transition-[opacity,visibility] duration-300 lg:visible lg:relative lg:top-0 lg:z-0 lg:flex lg:h-full lg:w-auto lg:items-stretch lg:overflow-visible lg:bg-white/0 lg:px-0 lg:py-0 lg:pt-0 lg:opacity-100">
-              <NavLink to="/" className="flex items-stretch">
-                <li className="flex items-center gap-2 py-4 transition-colors duration-300 focus:bg-emerald-50 focus:outline-none focus-visible:outline-none lg:px-8">
-                  <span>Home</span>
-                </li>
-              </NavLink>
-              <NavLink to="/about" className="flex items-stretch">
-                <li className="flex items-center gap-2 py-4 transition-colors duration-300 focus:bg-emerald-50 focus:outline-none focus-visible:outline-none lg:px-8">
-                  <span>About</span>{" "}
-                </li>
-              </NavLink>
-              <NavLink to="/biography" className="flex items-stretch">
-                <li className="flex items-center gap-2 py-4 transition-colors duration-300 focus:bg-emerald-50 focus:outline-none focus-visible:outline-none lg:px-8">
-                  <span>Biography</span>{" "}
-                </li>
-              </NavLink>
-              <NavLink to="/albums" className="flex items-stretch">
-                <li className="flex items-center gap-2 py-4 transition-colors duration-300 focus:bg-emerald-50 focus:outline-none focus-visible:outline-none lg:px-8">
-                  <span>Albums</span>{" "}
-                </li>
-              </NavLink>
-              <NavLink to="/gallery" className="flex items-stretch">
-                <li className="flex items-center gap-2 py-4 transition-colors duration-300 focus:bg-emerald-50 focus:outline-none focus-visible:outline-none lg:px-8">
-                  <span>Gallery</span>{" "}
-                </li>
-              </NavLink>
-              <NavLink to="/socials" className="flex items-stretch">
-                <li className="flex items-center gap-2 py-4 transition-colors duration-300 focus:bg-emerald-50 focus:outline-none focus-visible:outline-none lg:px-8">
-                  <span>Socials</span>{" "}
-                </li>
-              </NavLink>
-              <LoginBtn />
-              <LogoutBtn />
-            </ul>
+            {!error && !isLoading && (
+              <ul className="max-lg:left-72 max-lg:border-2 opacity-0 max-lg:z-20 max-lg:hover:opacity-100 max-lg:min-w absolute top-0 left-0 z-[-1] ml-auto h-screen w-full justify-center overflow-y-auto overscroll-contain bg-white/90 px-8 pb-12 pt-28 font-medium transition-[opacity,visibility] duration-300 lg:visible lg:relative lg:top-0 lg:z-0 lg:flex lg:h-full lg:w-auto lg:items-stretch lg:overflow-visible lg:bg-white/0 lg:px-0 lg:py-0 lg:pt-0 lg:opacity-100">
+                <NavLink to="/" className="flex items-stretch">
+                  <li className="flex items-center gap-2 py-4 transition-colors duration-300 focus:bg-emerald-50 focus:outline-none focus-visible:outline-none lg:px-8">
+                    <span>Home</span>
+                  </li>
+                </NavLink>
+                {isAuthenticated && (
+                  <>
+                    <NavLink to="/about" className="flex items-stretch">
+                      <li className="flex items-center gap-2 py-4 transition-colors duration-300 focus:bg-emerald-50 focus:outline-none focus-visible:outline-none lg:px-8">
+                        <span>About</span>{" "}
+                      </li>
+                    </NavLink>
+                    <NavLink to="/biography" className="flex items-stretch">
+                      <li className="flex items-center gap-2 py-4 transition-colors duration-300 focus:bg-emerald-50 focus:outline-none focus-visible:outline-none lg:px-8">
+                        <span>Biography</span>{" "}
+                      </li>
+                    </NavLink>
+                    <NavLink to="/albums" className="flex items-stretch">
+                      <li className="flex items-center gap-2 py-4 transition-colors duration-300 focus:bg-emerald-50 focus:outline-none focus-visible:outline-none lg:px-8">
+                        <span>Albums</span>{" "}
+                      </li>
+                    </NavLink>
+                    <NavLink to="/gallery" className="flex items-stretch">
+                      <li className="flex items-center gap-2 py-4 transition-colors duration-300 focus:bg-emerald-50 focus:outline-none focus-visible:outline-none lg:px-8">
+                        <span>Gallery</span>{" "}
+                      </li>
+                    </NavLink>
+                    <NavLink to="/socials" className="flex items-stretch">
+                      <li className="flex items-center gap-2 py-4 transition-colors duration-300 focus:bg-emerald-50 focus:outline-none focus-visible:outline-none lg:px-8">
+                        <span>Socials</span>{" "}
+                      </li>
+                    </NavLink>
+                  </>
+                )}
+                <LoginBtn />
+                <LogoutBtn />
+              </ul>
+            )}
 
-            <div className="flex items-center justify-end px-6 ml-auto lg:ml-0 lg:flex-1 lg:p-0">
-              <span className="inline-flex items-center justify-center gap-1 px-4 py-1 text-sm text-white bg-pink-500 rounded-full shadow-sm">
-                Trecia
-              </span>
-            </div>
+            {isAuthenticated && (
+              <div className="flex items-center justify-end px-6 ml-auto lg:ml-0 lg:flex-1 lg:p-0">
+                <User />
+              </div>
+            )}
           </nav>
         </div>
       </header>
