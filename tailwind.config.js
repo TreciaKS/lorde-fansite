30b490645750abe8035ac9167de5d264bfe1f7a2
Trecia Kat <tksekgalo@gmail.com>
2023-03-04 11:08:04 +0200

    updated grey colour

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
       "melo-sky-blue": "#2C99D2",
       "pure-white": "#E8E9E9",
       "pure-black": "#171818",
-      "pure-grey": "#A6A7A6",
+      "pure-grey": "#5C5C5C",
       "solar-brown": "#89450E",
       "solar-coffee": "#61493A",
       "solar-sea-blue": "#90BDE0",
