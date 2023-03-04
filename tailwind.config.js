84258d8c0360ea62d88a457027af3fcf9521986a
Trecia Kat <tksekgalo@gmail.com>
2023-03-04 12:07:31 +0200

    solar mustard added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,14 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     colors: {
-      "melo-night-blue": "#252637",
-      "melo-rainy-blue": "#49598E",
-      "melo-sky-blue": "#2C99D2",
+      "melo-night-blue": "#18152d",
+      "melo-rainy-blue": "#3a87d6",
+      "melo-sky-blue": "#19398d",
       "pure-white": "#E8E9E9",
       "pure-black": "#171818",
       "pure-grey": "#5C5C5C",
       "solar-brown": "#89450E",
-      "solar-coffee": "#61493A",
+      "solar-mustard": "#aa6d0a",
       "solar-sea-blue": "#90BDE0",
     },
     extend: {},
