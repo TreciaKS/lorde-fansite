63af1a288cdd7e516904bb1d79f899e87b4dfd37
Trecia Kat <tksekgalo@gmail.com>
2023-03-05 11:03:48 +0200

    styled socials page

--- src/pages/Socials.tsx
+++ src/pages/Socials.tsx
@@ -1,41 +1,45 @@
-import React from 'react'
+import { Player } from "@lottiefiles/react-lottie-player";
 
 const Socials = (): JSX.Element => {
   return (
-    <section>
-      <div className="flex items-center justify-center h-screen">
+    <section className="overflow-hidden bg-pure-white">
+      <div className="flex items-center justify-center mb-16 h-fit">
         <div className="flex-1 lg:-mx-6 lg:flex lg:items-center">
-          <div className="flex w-3/5 mx-auto mb-8 lg:mx-8 lg:w-1/2">
-            <div className="object-contain w-full px-8 py-10 mx-auto overflow-hidden lg:max-w-xl">
-              <img
-                className="h-full sm:min-w-96"
-                alt=""
-                loading="lazy"
-                src="./images/lorde_yellow_transparent.png"
-              />
+          <div className="flex w-3/5 mx-auto mb-8 lg:mx-8 lg:w-2/4">
+            <div className="object-contain w-full px-8 py-10 mx-auto border-none lg:max-w-xl bg-pure-white">
+              <Player
+                autoplay
+                loop={true}
+                src="https://assets6.lottiefiles.com/packages/lf20_eggvket4.json"
+                style={{
+                  height: "500px",
+                  width: "500px",
+                  backgroundColor: "transparent",
+                }}
+              ></Player>
             </div>
           </div>
 
-          <div className="text-center text-pure-grey lg:mx-6 lg:w-1/2">
-            <h1 className="text-2xl font-semibold capitalize lg:text-3xl">
-              Get a quote
+          <div className="text-center text-pure-grey lg:mx-6">
+            <h1 className="text-2xl font-bold capitalize lg:text-3xl">
+              Find Lorde
             </h1>
-            <p className="mt-6">
-              Ask us everything and we would love to hear from you
+            <p className="mt-6 w-96">
+              If you'd like to see more of Lorde outside of music, then you better make sure you follow her. <span className="font-bold text-solar-brown">Disclaimer: She's not a social media fanatic</span>  
             </p>
 
             <div className="mt-6 md:mt-8">
-              <h3 className="text-pure-grey">Follow us</h3>
+              <h3 className="text-pure-grey">Follow Lorde On</h3>
 
-              <div className="-mx-1.5 mt-4 flex justify-center">
+              <div className="-mx-1.5 mt-4 flex justify-center items-center">
                 <a
-                  className="mx-1.5 transform text-gray-600 transition-colors duration-300"
+                  className="mx-1.5 transform transition-colors duration-300"
                   href="/"
                 >
                   <svg
-                    className="w-10 h-10 fill-current"
+                    className="w-10 h-10"
                     viewBox="0 0 24 24"
-                    fill="none"
+                    fill="text-pure-grey"
                     xmlns="http://www.w3.org/2000/svg"
                   >
                     <path d="M18.6668 6.67334C18.0002 7.00001 17.3468 7.13268 16.6668 7.33334C15.9195 6.49001 14.8115 6.44334 13.7468 6.84201C12.6822 7.24068 11.9848 8.21534 12.0002 9.33334V10C9.83683 10.0553 7.91016 9.07001 6.66683 7.33334C6.66683 7.33334 3.87883 12.2887 9.3335 14.6667C8.0855 15.498 6.84083 16.0587 5.3335 16C7.53883 17.202 9.94216 17.6153 12.0228 17.0113C14.4095 16.318 16.3708 14.5293 17.1235 11.85C17.348 11.0351 17.4595 10.1932 17.4548 9.34801C17.4535 9.18201 18.4615 7.50001 18.6668 6.67268V6.67334Z" />
@@ -43,32 +47,7 @@ const Socials = (): JSX.Element => {
                 </a>
 
                 <a
-                  className="mx-1.5 transform text-pure-grey transition-colors duration-300"
-                  href="/home/"
-                >
-                  <svg
-                    className="w-8 h-8"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M15.2 8.80005C16.4731 8.80005 17.694 9.30576 18.5941 10.2059C19.4943 11.1061 20 12.327 20 13.6V19.2H16.8V13.6C16.8 13.1757 16.6315 12.7687 16.3314 12.4687C16.0313 12.1686 15.6244 12 15.2 12C14.7757 12 14.3687 12.1686 14.0687 12.4687C13.7686 12.7687 13.6 13.1757 13.6 13.6V19.2H10.4V13.6C10.4 12.327 10.9057 11.1061 11.8059 10.2059C12.7061 9.30576 13.927 8.80005 15.2 8.80005Z"
-                      fill="currentColor"
-                    />
-                    <path
-                      d="M7.2 9.6001H4V19.2001H7.2V9.6001Z"
-                      fill="currentColor"
-                    />
-                    <path
-                      d="M5.6 7.2C6.48366 7.2 7.2 6.48366 7.2 5.6C7.2 4.71634 6.48366 4 5.6 4C4.71634 4 4 4.71634 4 5.6C4 6.48366 4.71634 7.2 5.6 7.2Z"
-                      fill="currentColor"
-                    />
-                  </svg>
-                </a>
-
-                <a
-                  className="mx-1.5 transform text-pure-grey transition-colors duration-300"
+                  className="mx-1.5 transform text-pure-black transition-colors duration-300"
                   href="/home/"
                 >
                   <svg
@@ -85,7 +64,7 @@ const Socials = (): JSX.Element => {
                 </a>
 
                 <a
-                  className="mx-1.5 transform text-pure-grey transition-colors duration-300"
+                  className="mx-1.5 transform text-pure-black transition-colors duration-300"
                   href="/home/"
                 >
                   <svg
@@ -107,6 +86,6 @@ const Socials = (): JSX.Element => {
       </div>
     </section>
   );
-}
+};
 
-export default Socials
+export default Socials;
