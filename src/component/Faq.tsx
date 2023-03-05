f924275afcd9f4eeae0d621de72670e076507451
Trecia Kat <tksekgalo@gmail.com>
2023-03-05 12:01:43 +0200

    fixed margin

--- src/component/Faq.tsx
+++ src/component/Faq.tsx
@@ -2,20 +2,39 @@ import { useState } from "react";
 import FaqData from "../data/faq.json";
 
 const Faq = () => {
-const [faqs, setFaqs] = useState(FaqData);
-    
+  const [faqs, setFaqs] = useState(FaqData);
+
   return (
-    <>
       <div className="faq">
-        {faqs.map((faq) => (
-          <div key={faq.id}>
-            <h2>{faq.question}</h2>
-            <p>{faq.answer}</p>
+        <div className="p-10 mx-auto bg-white rounded shadow-lg">
+          <h3 className="px-8 py-6 font-bold text-center bg-grey-lightest">
+            Frequently Asked Questions
+          </h3>
+
+          <div className="mx-4 mt-2 question-wrap">
+            {faqs.map((faq) => (
+              <details className="py-4 border-b question border-grey-lighter">
+                <summary className="flex items-center font-bold text-left">
+                  <h2>{faq.question}</h2>
+                  <button className="ml-auto">
+                    <svg
+                      className="w-4 h-4 -mr-1 opacity-75 fill-current"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                    >
+                      <path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z" />
+                    </svg>
+                  </button>
+                </summary>
+                <div className="mt-4 leading-normal text-left text-md">
+                  {faq.answer}
+                </div>
+              </details>
+            ))}
           </div>
-        ))}
+        </div>
       </div>
-    </>
-  );
-};
+  )
+}
 
 export default Faq;
