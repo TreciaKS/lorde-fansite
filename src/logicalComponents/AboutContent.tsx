49b34f3561b26e9d4da29e4a1f32c559318b89fa
Trecia Kat <tksekgalo@gmail.com>
2023-03-05 21:23:48 +0200

    fixed rmobile esponsive issues

--- src/logicalComponents/AboutContent.tsx
+++ src/logicalComponents/AboutContent.tsx
@@ -15,27 +15,27 @@ type Props = {
 // returning only an element
 const AboutContent = ({ abouts }: Props): ReactElement => {
   return (
-    <section className="mx-2 my-5 tracking-wide">
-      <div className="grid h-screen grid-rows-3 gap-3 text-black lg:grid-cols-4 md:grid-cols-2">
-        <article className="grid overflow-y-auto shadow-md rounded-2xl place-items-center lg:row-span-3 md:row-span-1 bg-melo-night-blue shadow-melo-night-blue">
+    <>
+      <div className="grid h-screen grid-rows-3 gap-3 mx-2 tracking-wide text-black max-sm:-pb-12 lg:grid-cols-4 md:grid-cols-2 max-md:-mb-14 max-sm:-mb-16 max-md:h-fit">
+        <article className="grid overflow-y-auto shadow-md rounded-2xl place-items-center lg:row-span-3 md:row-span-1 bg-melo-night-blue shadow-melo-night-blue max-md:bg-pure-white max-md:shadow-none ">
           <div className="px-1 py-10 mx-auto">
-            <h1 className="text-2xl text-center capitalize text-pure-white lg:text-3xl">
+            <h1 className="text-2xl font-bold text-center capitalize max-md:text-melo-night-blue text-pure-white lg:text-3xl">
               Let's talk favorite song
             </h1>
 
             <div className="flex justify-center mx-auto mt-6">
-              <span className="inline-block w-40 h-1 rounded-full bg-solar-mustard"></span>
+              <span className="inline-block w-40 h-1 rounded-full bg-solar-sea-blue"></span>
               <span className="inline-block w-3 h-1 mx-1 rounded-full bg-solar-brown"></span>
               <span className="inline-block w-1 h-1 rounded-full bg-solar-sea-blue"></span>
             </div>
 
-            <div className="flex items-start max-w-6xl mx-auto mt-16 max-md:mx-5">
+            <div className="flex items-start max-w-6xl mx-auto mt-16 max-md:mx-5 ">
               <div>
                 <div className="text-center text-pure-white lg:mx-6">
-                  <h1 className="mb-3 -mt-5 text-2xl font-semibold capitalize text-solar-sea-blue">
+                  <h1 className="mb-3 -mt-5 text-2xl font-bold capitalize text-solar-mustard">
                     {abouts.favoriteTrackTitle}
                   </h1>
-                  <p className="text-pure-white">
+                  <p className="text-pure-white max-md:text-melo-night-blue">
                     {abouts.favouriteTrackDescription}
                   </p>
                 </div>
@@ -47,7 +47,7 @@ const AboutContent = ({ abouts }: Props): ReactElement => {
                     <h1 className="font-semibold text-melo-rainy-blue">
                       Pure Heroin: Extended Version
                     </h1>
-                    <span className="text-sm text-pure-white">
+                    <span className="text-sm text-pure-white max-md:text-melo-night-blue">
                       Alternative | Indie Pop
                     </span>
                   </div>
@@ -56,7 +56,7 @@ const AboutContent = ({ abouts }: Props): ReactElement => {
             </div>
           </div>
         </article>
-        <article className="pt-12 overflow-y-scroll shadow-md rounded-xl bg-pure-black lg:col-span-2 lg:row-span-2 md:row-span-3 shadow-pure-black">
+        <article className="pt-12 overflow-y-scroll shadow-md rounded-xl bg-pure-black lg:col-span-2 lg:row-span-2 md:row-span-3 shadow-pure-black max-lg:place-items-center max-lg:grid">
           <div className="container px-1 mx-auto">
             <h1 className="text-2xl font-semibold text-center capitalize text-melo-rainy-blue lg:text-3xl">
               {abouts.title}
@@ -86,14 +86,14 @@ const AboutContent = ({ abouts }: Props): ReactElement => {
             </div>
           </div>
         </article>
-        <div className="grid object-contain w-full overflow-hidden border-4 shadow-md rounded-2xl lg:row-span-2 place-items-center max-md:hidden border-solar-mustard shadow-solar-mustard">
+        <div className="grid object-contain w-full p-0 overflow-hidden border-b-4 shadow-md rounded-2xl lg:row-span-2 place-items-center border-solar-mustard shadow-solar-mustard">
           <img
             src="./images/lorde_chill.png"
             alt="Lorde in black jacket, long brown hair and pinkish lipstick"
-            className="w-full rounded-b-full"
+            className="w-full"
           />
         </div>
-        <article className="relative object-contain overflow-hidden border-4 rounded-lg shadow-md lg:col-start-2 lg:col-span-3 lg:row-start-3 shadow-melo-sky-blue">
+        <article className="relative object-contain overflow-hidden border-b-4 rounded-lg shadow-md lg:col-start-2 lg:col-span-3 lg:row-start-3 shadow-melo-sky-blue max-md:h-fit max-md:mb-32">
           <iframe
             className="pl-10 pr-10 mx-auto mt-14"
             style={{
@@ -108,8 +108,8 @@ const AboutContent = ({ abouts }: Props): ReactElement => {
           ></iframe>
         </article>
       </div>
-    </section>
+    </>
   );
-};
+}
 
 export default AboutContent;
