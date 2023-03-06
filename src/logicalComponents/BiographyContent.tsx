b38d6b189cbf9ae468369a9670a055b48dd0a5cf
Trecia Kat <tksekgalo@gmail.com>
2023-03-06 13:53:55 +0200

    responsive on mobile

--- src/logicalComponents/BiographyContent.tsx
+++ src/logicalComponents/BiographyContent.tsx
@@ -13,34 +13,27 @@ type Props = {
 };
 const BiographyContent = ({ biography }: Props): JSX.Element => {
   return (
-    <main key={biography.id} className="grid h-screen place-items-center">
+    <main key={biography.id} className="grid h-fit place-items-center">
       <div className="grid grid-cols-1 gap-4 p-5 md:grid-cols-3">
-        <section className="relative col-span-1 font-bold text-center rounded-lg shadow-lg text-md md:col-span-2">
+        <section className="object-contain w-full max-lg:hidden max-md:block">
+          <img
+            className="relative object-cover h-full min-w-full max-md:object-top max-md:h-96"
+            src={biography.monarchImgURL}
+            alt="hello world"
+          />
+        </section>
+
+        <section className="relative col-span-1 font-bold text-center rounded-lg shadow-lg text-md max-lg:col-span-2 max-md:col-span-1">
           <h1 className="mb-4 text-lg text-black">
             <h2>{biography.title}</h2>
           </h1>
 
           <p>{biography.description}</p>
-          <button className="flex p-3 m-auto mt-3 -mb-4 border-2">
-            Click Me
-          </button>
         </section>
 
-        <section className="object-contain w-full h-96">
+        <section className="object-contain">
           <img
-            className="w-full h-full rounded-lg shadow-lg "
-            src={biography.monarchImgURL}
-            alt="hello world"
-          />
-        </section>
-        <section className="row-span-1 p-10 font-bold text-center rounded-lg shadow-lg text-md md:row-span-2">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut cumque
-          cum dolor non inventore ipsum, vel ullam asperiores quam numquam sit
-          voluptatibus dolores nobis fugiat quae est deleniti. Impedit, iusto!
-        </section>
-        <section>
-          <img
-            className="object-cover h-full min-w-full rounded-lg shadow-lg"
+            className="relative object-cover h-full min-w-full max-md:object-top max-md:h-96"
             src={biography.melodramaImgURL}
             alt="Hello lorde is the "
           />
@@ -56,6 +49,6 @@ const BiographyContent = ({ biography }: Props): JSX.Element => {
       </div>
     </main>
   );
-}
+};
 
 export default BiographyContent;
