6819f6cb4b76720fe40f48d40b4b17a246faa4ef
Trecia Kat <tksekgalo@gmail.com>
2023-03-08 21:11:32 +0200

    changed background colour

--- src/auth/User.tsx
+++ src/auth/User.tsx
@@ -7,7 +7,7 @@ const { user, isAuthenticated } = useAuth0()
   return (
     <>
       {isAuthenticated && (
-        <span className="inline-flex items-center justify-center gap-1 px-2.5 py-2 text-sm capitalize bg-pink-500 rounded-full shadow-md bg-melo-rainy-blue">
+        <span className="inline-flex items-center justify-center gap-1 px-2.5 py-2 text-sm capitalize bg-pink-500 rounded-full shadow-md bg-melo-sky-blue">
           {user?.nickname}
         </span>
       )}
