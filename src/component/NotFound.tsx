9710037be1cbfbe1130b7051d05f96aa7a7bf92f
Trecia Kat <tksekgalo@gmail.com>
2023-03-08 22:07:55 +0200

    404 page styled

--- src/component/NotFound.tsx
+++ src/component/NotFound.tsx
@@ -1,17 +1,28 @@
 import { Player } from "@lottiefiles/react-lottie-player";
+import { NavLink } from "react-router-dom";
 
-const NotFound = () : JSX.Element=> {
-
+const NotFound = (): JSX.Element => {
   return (
     <section className="grid h-screen place-items-center">
-      <Player
-        autoplay
-        loop
-        src="https://assets9.lottiefiles.com/packages/lf20_yCCORfUI6a.json"
-        style={{ height: "200px", width: "200px" }}
-      ></Player>
+      <div className="bg-pure-white">
+        <Player
+          autoplay
+          loop
+          src="https://assets9.lottiefiles.com/packages/lf20_yCCORfUI6a.json"
+          style={{ height: "500px", width: "500px" }}
+        ></Player>
+      </div>
+      <h1 className="p-0 text-lg font-semibold capitalize -mt-96">
+        Return to{" "}
+        <NavLink
+          to="/"
+          className={"underline underline-offset-4 hover:tracking-wide"}
+        >
+          home?
+        </NavLink>
+      </h1>
     </section>
   );
-}
+};
 
-export default NotFound
+export default NotFound;
